ec523162133dd9895b741064ebf8e870ffeccfbf
Adao Natalino <sansonov@gmail.com>
2021-05-11 08:09:48 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,15 @@
+def greeting
+  puts "Hello World"
+end
+
+#greeting
+
+def greeting_5_times
+greeting
+greeting
+greeting
+greeting
+greeting
+end
+
+greeting_5_times
---
+++ my_new_directory/README.md
@@ -0,0 +1,1 @@
+Adao Jose Domingues Natalino
